2ba2e631f780cc5b239993e549ff892af61ca876
Robert Pedersen <40696979+robpedersendev@users.noreply.github.com>
2020-10-13 01:48:16 -0600

    Background video finally is fully responsive. In addition I added the basic structure of HTML5 tags for the app.js body.

--- saftey101/src/App.js
+++ saftey101/src/App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import logo from "./logo.svg";
+import Matrix from "./Assets/matrix.mp4";
 import "./App.css";
 
 function App() {
+  const Video = () => 
+        (<video autoPlay muted loop disablePictureInPicture preload="auto" id="matrix" controlsList="nodownload">
+          <source src={Matrix} type="video/mp4" />
+          <source src={Matrix} type="video/ogg" />
+           Your browswer does not support the Video tag
+      </video>)
+  ;
   return (
     <div className="App">
+      <Video />
+      <header>
       
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
