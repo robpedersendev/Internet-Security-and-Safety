d1ac18a770e9a45cc71c1a6ae485d2c854de721b
Robert Pedersen <40696979+robpedersendev@users.noreply.github.com>
2020-10-27 23:09:14 -0600

    Added z-index to nav and adjsuted logo

--- Safety101/src/App.js
+++ Safety101/src/App.js
@@ -2,17 +2,22 @@ import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 import Matrix from "./Assets/matrix.mp4";
+import Switch from "./Assets/switch.jpg";
+import AudioServer from "./Assets/audio-server.jpg";
 import "./App.css";
 
 function App() {
   // This line will eventually be its own element. But this is how I am able to display the video
   const Video = () => 
-        (<video autoPlay muted loop disablePictureInPicture preload="auto" id="matrix" controlsList="nodownload">
+        (<video className="vidplaying" autoPlay muted playbackRate="0.1" loop disablePictureInPicture preload="auto" id="matrix" controlsList="nodownload">
           <source src={Matrix} type="video/mp4" />
           <source src={Matrix} type="video/ogg" />
            Your browswer does not support the Video tag
       </video>)
   ;
+
+  const [vid, updateVid] = useState([Video()]);
+
   return (
     <div className="App">
       {/* Full Screen, responsive video playing */}
@@ -26,15 +31,16 @@ function App() {
       </ul>
       
       </nav>
-      <nav className="main_nav">
-        {/* <img>Logo</img> */}
+      <nav  className="main_nav">
+        <img id="logo"  src={Switch}/>
       <ul>
         <NavLink             
         className="navbar-item"
         activeClassName="is-active"
-        to=""
+        to="/"
         >
-              Home</NavLink>
+        Home
+        </NavLink>
         <NavLink to="">About Us</NavLink>
         <NavLink to="">More Information</NavLink>
       </ul>
